fdbb17ae2bdd78bed8aa6723cc3a1638e58aa0a0
Magdy <129180170+MagdyGhazy@users.noreply.github.com>
2023-05-24 16:33:37 +0300

    Initial commit

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\About;
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $abouts = About::all();
+        return view('admin.about.about',compact('abouts'));
+
+    }
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+
+        return view('admin.abouts.cre',compact('abouts'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        About::create([
+           'title'=> $request->title,
+           'discribtion'=> $request->discribtion,
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\About  $about
+     * @return \Illuminate\Http\Response
+     */
+    public function show(About $about)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\About  $about
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(About $about)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\About  $about
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, About $about)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\About  $about
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(About $about)
+    {
+        //
+    }
+}
